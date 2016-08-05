3e284455130d5a7e7a908293249fe2606fcce44e
KrazyKevZ <kevin@crimsontechnik.com>
2016-08-05 22:34:36 +0800

    Add actions for firebase login and logout

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -1,6 +1,6 @@
 import moment from 'moment';
 
-import firebase, {firebaseRef} from 'app/firebase/';
+import firebase, {firebaseRef, githubProvider} from 'app/firebase/';
 
 //setSearchText
 export var setSearchText = (searchText) => {
@@ -89,5 +89,23 @@ export var startToggleTodo = (id, completed) => {
       dispatch(updateTodo(id, updates));
     });
 
+  };
+};
+
+export var startLogin = () => {
+  return (dispatch, getState) => {
+    return firebase.auth().signInWithPopup(githubProvider).then((result) => {
+      console.log('Auth worked!', result);
+    }, (error) => {
+      console.log('Unable to auth', error);
+    });
+  };
+};
+
+export var startLogout = () => {
+  return (dispatch, getState) => {
+    return firebase.auth().signOut().then(() => {
+      console.log('Logged out!');
+    });
   };
 };
